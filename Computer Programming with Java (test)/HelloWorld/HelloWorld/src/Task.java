8a39541b552aa06ebdebaf63be15e90a9de56b84
SkaviCos <skavinski90@mail.ru>
2018-11-14 23:12:49 +0300

    Delete Files

--- Computer Programming with Java (test)/HelloWorld/HelloWorld/src/Task.java
+++
@@ -1,4 +0,0 @@
-class Task {
-  //Write all necessary code here to make the Hello World! message appear
-  type here
-}
--- Introduction to Python/Strings/String length/len_function.py
+++
@@ -1,7 +0,0 @@
-phrase = """
-It is a really long string
-triple-quoted strings are used
-to define multi-line strings
-"""
-first_half = len(phrase)
-print(first_half)
